ce203bd3dd5fdfa9b6be730c06c8952df65f3879
shivani <shivani.reddy00@gmail.com>
2019-12-20 14:50:10 +0530

    Adding Project Files

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static('frontend'))
+
+app.get('/', (req, res) => res.sendFile(__dirname+'/frontend/html/home.html'))
+app.get('/login', (req, res) => res.send('Login'))
+app.get('/register', (req, res) => res.send('Register'))
+var student = {"name" : "Shivani",
+               "college" : "VNRVJIET",
+                "regno" : "17071A1209"}
+                app.get('/data', (req, res)=> res.json(student));
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
